c9c63a276f2b772afeceb43fdc11b91686f5e5a9
Vitaly Shvachka <47694214+VitaliyShvachka@users.noreply.github.com>
2024-01-15 17:45:58 +0100

    add user_id in teams

--- database/migrations/2024_01_12_000004_create_teams_table.php
+++ database/migrations/2024_01_12_000004_create_teams_table.php
@@ -14,6 +14,8 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
         });
     }
@@ -25,6 +27,9 @@ class CreateTeamsTable extends Migration
      */
     public function down()
     {
+        Schema::table('boards', function (Blueprint $table){
+          $table->dropForeign('teams_user_id_foreign');
+        });
         Schema::dropIfExists('teams');
     }
 }
