e760990bf0a8c2738441ef055299650fe45b9548
walbrat <walbrat@gmail.com>
2024-01-16 10:45:02 +0200

    left section init

--- resources/views/partials/left-section.blade.php
+++ resources/views/partials/left-section.blade.php
@@ -6,13 +6,9 @@
 </style>
 
 <section class="left-section">
- <h2>Заголовок секции 1</h2>
- <button class="btn btn-primary">Добавить команду</button>
- <ul id="task-list">
-  <li>Задача 1</li>
-  <li>Задача 2</li>
-  <li>Задача 3</li>
- </ul>
+ @include('partials.leftSection.tasks')
+ @include('partials.leftSection.teams')
+
 </section>
 
 
