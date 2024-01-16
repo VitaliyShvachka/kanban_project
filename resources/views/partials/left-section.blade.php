a268ce4aaa8297fb195f9eda43f408888e5d3cfa
walbrat <walbrat@gmail.com>
2024-01-16 10:14:16 +0200

    init patern

---
+++ resources/views/partials/left-section.blade.php
@@ -0,0 +1,19 @@
+<style>
+ .left-section {
+  float: left;
+  width: 20%;
+ }
+</style>
+
+<section class="left-section">
+ <h2>Заголовок секции 1</h2>
+ <button class="btn btn-primary">Добавить команду</button>
+ <ul id="task-list">
+  <li>Задача 1</li>
+  <li>Задача 2</li>
+  <li>Задача 3</li>
+ </ul>
+</section>
+
+
+
---
+++ resources/views/partials/right-section.blade.php
@@ -0,0 +1,25 @@
+<style>
+ .right-section {
+  float: left;
+  width: 80%;
+  margin-left: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+ }
+ .column {
+  width: 30%;
+  padding: 10px;
+  background-color: #f2f2f2;
+  margin-bottom: 10px;
+ }
+</style>
+
+
+<section class="right-section">
+
+ <div class="column">Колонка 1</div>
+ <div class="column">Колонка 2</div>
+ <div class="column">Колонка 3</div>
+</section>
+
