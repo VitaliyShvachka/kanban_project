6e0ec0932e75326e11f505568c348b3c05555db4
OpeR <pit-stop@3g.ua>
2024-01-19 09:53:25 +0200

    TeamController change

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,19 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
+use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests;
 
 class TeamController extends Controller
 {
     public function create()
     {
-      //відобразити форму створення команди
+        $data = [];
+        $data['logins'] = User::pluck('login');
+        dump($data);
+        return view('team.create', $data);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-      // додаємо команду в базу
+        // Правила валидации
+        $rules = [
+            'name' => 'required|string|max:255', // Пример правила для уникального имени
+        ];
+        // Сообщения об ошибках
+        $messages = [
+            'name.required' => 'Вкажіть назву команди',
+            'name.string' => 'Вкажіть назву команди одим рядком',
+            'name.max' => 'Назва не може бути довшою ніж 255 сиимволів',
+        ];
+        // Валидація
+        $validator = Validator::make($request->all(), $rules, $messages);
+        // Проверка на наличие ошибок
+        if ($validator->fails()) {
+            return redirect('/team/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $team = new Team();
+        $team->name = $request->name;
+        $team->save();
+
+        return redirect('/')
+            ->with('success', 'Команда успішно створена.');
     }
 }
