03d0e5ffe2e690c1a2386a9f3c1cf81cfb74a9e0
OpeR <pit-stop@3g.ua>
2024-01-19 09:54:34 +0200

    Team rotes change

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::auth();
 //Route::get('/home', 'HomeController@index');
  // TeamController
 Route::get('/team/create', 'TeamController@create');
-Route::post('/team', 'TeamController@store');
+Route::post('/team', 'TeamController@store')->name('team.store');
 
 // BoardController
 Route::get('/board/show/{board}', 'BoardController@show');
