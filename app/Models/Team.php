b273bc419575e1050fbf1b467595d265c93b0f6e
OpeR <pit-stop@3g.ua>
2024-01-23 01:56:29 +0200

    model Team create method users

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['name','user_id'];
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'teams_user', 'team_id', 'user_id');
+    }
 }
