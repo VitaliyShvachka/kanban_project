296737fb4aabd1adf6960815361cb49aaeb5ce3b
OpeR <pit-stop@3g.ua>
2024-01-29 02:19:09 +0200

    teams blades page chage

--- resources/views/partials/leftSection/teams.blade.php
+++ resources/views/partials/leftSection/teams.blade.php
@@ -1,8 +1,8 @@
-<a href="{{ route('team.create') }}" class="btn btn-primary">Додати команду</a>
+<a href="{{ route('team.create') }}" class="btn btn-primary">{{ trans('team.head-home-sidbar') }}</a>
 <ul id="task-list">
-@foreach($teams as $team)
+    @foreach($teams as $team)
         <li>{{$team->name}}
-                <a href="{{ route('board.create', ['$team' => $team->id]) }}" title="">[+ Дошка]</a>
+            <a href="{{ route('board.create', ['$team' => $team->id]) }}" title=""><i class="fa fa-plus"></i> {{ trans('team.add-board-btn-sidbar') }}</a>
         </li>
-@endforeach
+    @endforeach
 </ul>
