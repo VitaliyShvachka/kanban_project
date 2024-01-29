fd9ca93430d9d1a4bc9912daa544e493ab87c910
OpeR <pit-stop@3g.ua>
2024-01-29 02:24:55 +0200

    team.php  lang add

---
+++ resources/lang/en/team.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Team forms created Language Lines
+    |--------------------------------------------------------------------------
+
+    |
+    */
+    'head-home-sidebar' => 'Add Team',
+    'add-board-btn-sidebar' => 'Board',
+
+    'heading-create' => 'Create a new team',
+    'name-team' => 'Team name',
+    'btn-submit-create' => 'Next',
+
+    'heading' => 'Adding members to the team: ',
+    'login' => 'Login',
+    'btn-submit' => 'Submit',
+    'users-team' => 'Team members: ',
+    'id' => 'id',
+    'name' => "Name",
+    'finish-bn' => 'Finish',
+];
