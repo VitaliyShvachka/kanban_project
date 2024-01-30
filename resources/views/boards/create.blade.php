f0769b71905df6d6b93369323cf3c272ab73a8d8
OpeR <pit-stop@3g.ua>
2024-01-30 15:31:50 +0200

    Створення дошки. Зміна шаблона

--- resources/views/boards/create.blade.php
+++ resources/views/boards/create.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
+
 @section('content')
-<form action="{{route('board.store', ['$team' => $team->id])}}" method="post">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <label for="name" class="col-md-4 control-label">Board</label>
-    <input id="name" type="text" class="form control" name="title"/>
-    {{--    <input type="hidden" name="id_team" value="{{$idTeam}}"/>--}}
-    <input type="hidden" name="id_team" value="1"/>
-    <input type="submit" value="Зберегти"/>
-</form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Створення дошки</div>
+                    <div class="panel-body">
+                        <form action="{{route('board.store', ['$team' => $team->id])}}" method="post" class="form-horizontal">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <label for="name" class="col-md-4 control-label">Назва</label>
+                            <input id="name" type="text" class="form control" name="title"/>
+                            {{--    <input type="hidden" name="id_team" value="{{$idTeam}}"/>--}}
+                            <input type="hidden" name="id_team" value="1"/>
+                            <input type="submit" value="Зберегти" class="btn btn-primary"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
