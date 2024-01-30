6efe38daacde73a842cb705c19b75ac829e8f10c
OpeR <pit-stop@3g.ua>
2024-01-30 15:08:35 +0200

    Змінено шаблон Дошок

--- resources/views/partials/leftSection/boards.blade.php
+++ resources/views/partials/leftSection/boards.blade.php
@@ -1,12 +1,21 @@
-<h4>Список дошок користувача</h4>
-<ul id="task-list">
-    @foreach($teams as $team)
+<div class="well well-sm">
+    <h3>Дошки</h3>
+    <div class="list-group">
+        @foreach($teams as $team)
             @foreach($team->boards as $board)
-                <li>
-                    <a href="{{route('board.show', ['$board' => $board->id])}}" title="">{{$board->title}}</a>
-                    <a href="{{route('task.create', ['board' => $board->id])}}" class="btn btn-default">Додати задачу</a>
-                </li>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <a href="{{route('board.show', ['$board' => $board->id])}}" title="">{{$board->title}}</a>
+                        </div>
+                        <div class="col-sm-6 text-right">
+                            <a href="{{route('task.create', ['board' => $board->id])}}" class="btn btn-warning"><i
+                                        class="fa fa-plus"></i> Додати задачу</a>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-    @endforeach
-</ul>
+        @endforeach
+    </div>
+</div>
 
