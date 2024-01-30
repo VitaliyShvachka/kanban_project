7d4a4e0f0a2504376e5ef4c805bf515c3ca8604f
OpeR <pit-stop@3g.ua>
2024-01-30 15:26:58 +0200

    Створення команди. Зміна шаблона

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,14 +1,38 @@
-<form action="{{route('task.store')}}" method="post">
-    {{ csrf_field() }}
-    <label for="name" class="col-md-4 control-label">Назва</label>
-    <input id="name" type="text" class="form control" name="name"/>
-    <select multiple name="members[]">
-        @foreach($members as $item)
-            <option>{{$item}}</option>
-        @endforeach
-    </select>
-    <input type="hidden" name="board_id" value="{{$board->id}}">
-    <label for="description" class="col-md-4 control-label">Задача</label>
-    <input id="description" type="text" class="form control" name="description"/>
-    <input type="submit" value="Створити"/>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Створення задачі</div>
+                    <div class="panel-body">
+                        <form action="{{route('task.store')}}" method="post" >
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="name" class="control-label">Назва</label>
+                                <input id="name" type="text" class="form-control" name="name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="members" class="control-label">Учасники</label>
+                                <select id="members" multiple name="members[]" class="form-control">
+                                    @foreach($members as $item)
+                                        <option>{{$item}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="hidden" name="board_id" value="{{$board->id}}">
+                                <label for="description" class="control-label">Задача</label>
+                                <input id="description" type="text" class="form-control" name="description"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Створити" class="btn btn-primary"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
