9533e5ffb43c31f4eae4fb2699aed8fa13f896ba
OpeR <pit-stop@3g.ua>
2024-01-30 15:10:15 +0200

    Футер змінено шаблон

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,17 +1,9 @@
-<style>
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: #f2f2f2;
-        color: black;
-        text-align: center;
-        padding: 10px;
-    }
-</style>
-<footer class="footer">
-    <p>Копирайт © 2024</p>
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="well well-sm">
+                <p>Копирайт © 2024</p>
+            </div>
+        </div>
+    </div>
 </footer>
-
-
