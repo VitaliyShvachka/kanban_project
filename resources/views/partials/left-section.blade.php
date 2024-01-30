7f997063bf61f45a6e8ab408b21aad8eae86baec
OpeR <pit-stop@3g.ua>
2024-01-30 15:10:45 +0200

    Ліва секція зміна шаблона

--- resources/views/partials/left-section.blade.php
+++ resources/views/partials/left-section.blade.php
@@ -1,15 +1,8 @@
-<style>
- .left-section {
-  float: left;
-  width: 20%;
- }
-</style>
-
-<section class="left-section">
- @include('partials.leftSection.teams')
- @include('partials.leftSection.boards')
-
-</section>
-
-
-
+<div class="container-fluid">
+    <div class="row">
+        <section class="left-section">
+            @include('partials.leftSection.teams')
+            @include('partials.leftSection.boards')
+        </section>
+    </div>
+</div>
